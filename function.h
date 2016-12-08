96d572ca64d0f45d6e28df193f1d650c0b61bb90
Barry Shi <major630@163.com>
2016-12-08 10:17:56 +0800

    Add files via upload

---
+++ function.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include <stdio.h>
+#include <cmath>
+
+extern double sqrtTable1[];
+extern double sqrtTable2[];
+extern double sqrtTable3[];
+extern double acosTable[];
+extern int sinTable[];
+extern int cosTable[];
+
+#endif
